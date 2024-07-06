bb2c7fea276659b0a5fd47fe924c5c8145de142d
EliteB0Y <itseliteboy@gmail.com>
2024-07-06 14:07:39 +0530

    Fixed: Merge Issue

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask
 from threading import Thread
 
@@ -14,21 +13,4 @@ def run():
 
 def keep_alive():
   server = Thread(target=run)
-=======
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-
-@app.route('/')
-def main():
-  return "Your bot is alive!"
-
-def run():
-  app.run(host="0.0.0.0", port=8080)
-
-def keep_alive():
-  server = Thread(target=run)
->>>>>>> master
   server.start()
